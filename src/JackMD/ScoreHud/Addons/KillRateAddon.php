88c6ac12adb23c3517a9f9eb00f24934de57f345
xiaomipadmikula-alt <xiaomipadmikula@gmail.com>
2026-03-14 17:40:52 +0500

    KillRateAddon.php

--- src/JackMD/ScoreHud/Addons/KillRateAddon.php
+++ src/JackMD/ScoreHud/Addons/KillRateAddon.php
@@ -1,1 +1,31 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace JackMD\ScoreHud\Addons;
+
+use JackMD\ScoreHud\Addons\AddonBase;
+use KillRate\KillRate;
+use pocketmine\player\Player;
+
+class KillRateAddon extends AddonBase{
+
+	/** @var KillRate */
+	private $killRate;
+
+	public function onEnable(): void{
+		$this->killRate = $this->getApi()->getPluginManager()->getPlugin("KillRate");
+	}
+
+	public function getHandlers(): array{
+		return [
+			"killrate.kille"  => function(Player $player){
+				return $this->killRate->getKillManager()->getPlayerKills($player);
+			},
+			"killrate.deaths" => function(Player $player){
+				return $this->killRate->getKillManager()->getPlayerDeaths($player);
+			}
+		];
+	}
+}
 
